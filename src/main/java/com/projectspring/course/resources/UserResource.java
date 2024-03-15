ac41c3701e558abba851ee83dbf047e76d3bc5da
Marcello Joaquim da Silva <marcello.silva@compasso.com.br>
2024-03-14 22:59:48 -0300

    implemented exceptions class ffor User methods delete and find by id

--- src/main/java/com/projectspring/course/resources/UserResource.java
+++ src/main/java/com/projectspring/course/resources/UserResource.java
@@ -1,14 +1,20 @@
 package com.projectspring.course.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projectspring.course.entities.User;
 import com.projectspring.course.services.UserService;
@@ -29,10 +35,31 @@ public class UserResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
-		Optional<User> obj = service.findById(id);
+		Optional<User> obj = Optional.of(service.findById(id));
 		
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
+	}
 	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@RequestBody User obj, @PathVariable Long id){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
--- src/main/java/com/projectspring/course/services/UserService.java
+++ src/main/java/com/projectspring/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.projectspring.course.entities.User;
 import com.projectspring.course.repositories.UserRepository;
+import com.projectspring.course.services.exceptions.DatabaseException;
+import com.projectspring.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +23,41 @@ public class UserService {
 		return repository.findAll();
 	}
 	
-	public Optional<User> findById(Long id) {
-		return repository.findById(id);
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			if(repository.existsById(id)) {
+			repository.deleteById(id);
+			} else {
+				throw new ResourceNotFoundException(id);
+			}
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		} 
+		
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
+	
+	
 }
