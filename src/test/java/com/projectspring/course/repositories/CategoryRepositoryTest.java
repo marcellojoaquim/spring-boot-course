11745a0ef25aed65917dd16d6e23bbb2d1f1121a
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2024-07-23 10:22:23 -0300

    setup tests

---
+++ src/test/java/com/projectspring/course/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.projectspring.course.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByName() {
+    }
+}
